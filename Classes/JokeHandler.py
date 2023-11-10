d7da2bc64d0d71e1cdd106cc85bd47ee5e6129d2
Cohnny <Tjonk11@gmail.com>
2023-11-10 03:57:22 +0100

    Added bot.py Added JokeHandler.py Added responses.py

--- Classes/JokeHandler.py
+++ Classes/JokeHandler.py
@@ -1,3 +1,17 @@
-class JokeHandler:
-    def __int__(self):
-        pass
+from jokeapi import Jokes
+
+
+async def print_joke(message):
+    j = await Jokes()
+    blacklist = ['racist']
+    if message.channel.is_nsfw():
+        blacklist.append("nsfw")
+    joke = await j.get_joke(blacklist=blacklist)
+    if joke["type"] == "single":
+        msg = joke["joke"]
+    else:
+        msg = joke["setup"]
+        msg += f"|| {joke['delivery']} ||"
+
+    return msg
+
--- bot.py
+++ bot.py
@@ -1,11 +1,35 @@
 import discord
 import responses
+from Classes import JokeHandler
+
+
+async def clear(ctx, amount: int):
+    print(ctx)
+    print(amount)
+    if not amount:
+        await ctx.send("Please provide a valid number of messages to clear.")
+        return
+
+    # Check if the user has permission to manage messages (adjust this as needed)
+    if ctx.author.guild_permissions.manage_messages:
+        await ctx.channel.purge(limit=amount + 1)  # +1 to include the command message
+        await ctx.send(f"{amount} messages cleared by {ctx.author.mention}.", delete_after=5)
+    else:
+        await ctx.send("You don't have permission to manage messages.")
 
 
 async def send_message(message, user_message, is_private):
     try:
         response = responses.handle_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+async def send_joke(message):
+    try:
+        response = await JokeHandler.print_joke(message)
+        await message.channel.send(response)
     except Exception as e:
         print(e)
 
@@ -37,5 +61,16 @@ def run_discord_bot():
         else:
             await send_message(message, user_message, is_private=False)
 
+        if user_message[0] == '!':
+            if user_message.startswith('!clear'):  # Check if the message starts with "!clear"
+                print("Inne i clear")
+                user_message = user_message[len('!clear'):]  # Remove "!clear" from the message
+            if user_message.strip():  # Check if there is a valid integer following the command
+                print("Inne i strip")
+                await clear(message, int(user_message.strip()))
+            else:
+                user_message = user_message[1:]
+                if user_message == 'joke':
+                    await send_joke(message)
 
     client.run(TOKEN)
