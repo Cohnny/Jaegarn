eb36fb61e083836e006ee75521aa694b1fc19405
Cohnny <Tjonk11@gmail.com>
2023-11-10 05:26:44 +0100

    Added bot.py Added JokeHandler.py Added responses.py

--- bot.py
+++ bot.py
@@ -1,76 +1,62 @@
 import discord
 import responses
+import requests
 from Classes import JokeHandler
+from discord.ext import commands
+from datetime import datetime
+from discord.http import Route
+
+TOKEN = 'MTE3MjI4NTU2MDgwMzU1NzQzNg.Go5xI6.R2XXxSpEeY3LkIaNT-HhkRJ3Zs0bkyo1QiiTjM'
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 
-async def clear(ctx, amount: int):
-    print(ctx)
-    print(amount)
-    if not amount:
-        await ctx.send("Please provide a valid number of messages to clear.")
+@bot.event
+async def on_ready():
+    print(f'{bot.user} is now running!')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
         return
 
-    # Check if the user has permission to manage messages (adjust this as needed)
-    if ctx.author.guild_permissions.manage_messages:
-        await ctx.channel.purge(limit=amount + 1)  # +1 to include the command message
-        await ctx.send(f"{amount} messages cleared by {ctx.author.mention}.", delete_after=5)
+    if message.channel.type == discord.ChannelType.private:
+        await send_message(message, message.content, is_private=True)
     else:
-        await ctx.send("You don't have permission to manage messages.")
+        await bot.process_commands(message)
+
+
+@bot.command()
+async def greet(ctx):
+    greeting = "Hello, I am your friendly bot!"
+    await ctx.send(greeting)
+
+
+@bot.command()
+async def joke(ctx):
+    try:
+        response = await JokeHandler.print_joke(ctx)
+        await ctx.send(response)
+    except Exception as e:
+        print(e)
+
+
+@bot.command()
+async def time(ctx):
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    await ctx.send(f"The current date and time is: {current_time}")
 
 
 async def send_message(message, user_message, is_private):
     try:
         response = responses.handle_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
-
-
-async def send_joke(message):
-    try:
-        response = await JokeHandler.print_joke(message)
-        await message.channel.send(response)
+        destination = message.author if is_private else message.channel
+        await destination.send(response)
     except Exception as e:
         print(e)
 
 
 def run_discord_bot():
-    TOKEN = 'MTE3MjI4NTU2MDgwMzU1NzQzNg.Go5xI6.R2XXxSpEeY3LkIaNT-HhkRJ3Zs0bkyo1QiiTjM'
-    intents = discord.Intents.default()
-    intents.message_content = True
-    client = discord.Client(intents=intents)
-
-    @client.event
-    async def on_ready():
-        print(f'{client.user} is now running!')
-
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-
-        username = str(message.author)
-        user_message = str(message.content)
-        channel = str(message.channel)
-
-        print(f'{username} said: "{user_message}" ({channel})')
-
-        if user_message[0] == '?':
-            user_message = user_message[1:]
-            await send_message(message, user_message, is_private=True)
-        else:
-            await send_message(message, user_message, is_private=False)
-
-        if user_message[0] == '!':
-            if user_message.startswith('!clear'):  # Check if the message starts with "!clear"
-                print("Inne i clear")
-                user_message = user_message[len('!clear'):]  # Remove "!clear" from the message
-            if user_message.strip():  # Check if there is a valid integer following the command
-                print("Inne i strip")
-                await clear(message, int(user_message.strip()))
-            else:
-                user_message = user_message[1:]
-                if user_message == 'joke':
-                    await send_joke(message)
-
-    client.run(TOKEN)
+    bot.run(TOKEN)
